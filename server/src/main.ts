69d9c518a2f509393cbd2c8fcca12552379f5c82
ManrrubiaF <manrrubiaf@gmail.com>
2023-09-21 00:08:45 -0300

    main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,10 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {  
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://www.favio-manrrubia.com.ar',"https://favio-manrrubia.com.ar"],
+    origin: 'https://www.favio-manrrubia.com.ar',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-    
+
   })
   await app.listen(3000);
 }

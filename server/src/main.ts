01ad85d640ace3016b87604d56b20e989adfdc82
ManrrubiaF <manrrubiaf@gmail.com>
2023-09-21 21:11:51 -0300

    another try

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || "3000";
 
 async function bootstrap() {  
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,6 @@ async function bootstrap() {
     credentials: true,
 
   })
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
